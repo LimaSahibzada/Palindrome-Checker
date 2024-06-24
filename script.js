98796bc7de7b04753ce833a98a06a48c563dec38
LimaSahibzada <lima.sahibzada2023@gmail.com>
2024-06-24 16:06:25 +0430

    removed double quoate and some spaces for clear code

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-document.getElementById("check-btn").addEventListener("click", function() {
-    var input = document.getElementById("text-input").value.trim(); 
-    var result = document.getElementById("result");
-    
-    if (input === "") {
-        alert("Please input a value");
-    } else {
-        var cleanedInput = input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-        var reversedInput = cleanedInput.split("").reverse().join("");
-  
-        if (cleanedInput === reversedInput) {
-            result.textContent = input + " is a palindrome";
-            result.style.color = "black"; 
-        } else {
-            result.textContent = input + " is not a palindrome";
-            result.style.color = "red"; 
-        }
-    }
-  });
+document.getElementById('check-btn').addEventListener('click', () => {
+  const input = document.getElementById('text-input').value.trim();
+  const result = document.getElementById('result');
 
-  document.getElementById("dark-mode-toggle").addEventListener("change", function() {
-    document.body.classList.toggle("dark-mode");
-  });
-  
+  if (input === '') {
+    alert('Please input a value');
+  } else {
+    const cleanedInput = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedInput = cleanedInput.split('').reverse().join('');
+
+    if (cleanedInput === reversedInput) {
+      result.textContent = `${input} is a palindrome`;
+      result.style.color = 'black';
+    } else {
+      result.textContent = `${input} is not a palindrome`;
+      result.style.color = 'red';
+    }
+  }
+});
+
+document.getElementById('dark-mode-toggle').addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
+});
